6608f5c08cccb00267e2e1bec8a389e478fd5b51
Marcel <marcelsenarios@gmail.com>
2018-04-16 18:38:00 -0300

    Inicio Cangaceiro Javascript

---
+++ cangaceiro-javascript/client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._valor;
+    }    
+
+    getValor() {
+        return this._valor;
+    }
+        
+}
